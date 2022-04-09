021ac6137f241166e9bfe015cab0d7e5d3912ae9
ErMapsh <72149385+ErMapsh@users.noreply.github.com>
2022-04-09 14:33:07 +0530

    facebook clone done

---
+++ Projects/FacebookClone/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        login: 'rgb(24,119,242)',
+        FP: 'rgb(24,119,242)',
+        bg: 'rgb(240,242,245)',
+        CB: 'rgb(54,164,32)'
+      },
+      width:{
+        45: '45%',
+      },
+
+
+    },
+  },
+  plugins: [],
+}
